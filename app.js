3155f98c1da5ed487ffd5e476099a4d93e1ad92a
ju5tu5 <justus@ju5tu5.nl>
2022-01-22 07:41:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const mysql = require('mysql2')
 // create the connection to database
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
