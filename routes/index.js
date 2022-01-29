c47fd323d4999c1c37ae48446d7b4817cab92c57
ju5tu5 <justus@ju5tu5.nl>
2022-01-29 13:34:28 +0100

    Changed /

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,18 @@ const openapi = require('../docs/openapi.json')
 module.exports = express
   .Router()
 
+  // Send openapi doc as formatted HTML
   .get('/', (req, res) => {
+    // parse to HTML?
+    res.json({
+      message: 'Welcome to tribe.api.fdnd.nl! Please use the resources below to expore this API.',
+      github: 'https://github.com/fdnd-apis/tribe',
+      spec: 'https://tribe.api.fdnd.nl/v1',
+      docs: 'https://redocly.github.io/redoc/?url=https://tribe.api.fdnd.nl/v1',
+    })
+  })
+
+  // Send openapi doc as json
+  .get('/v1', (req, res) => {
     res.json(openapi)
   })
